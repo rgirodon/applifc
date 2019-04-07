5f1300ff2d062c1f80025c6aca70060401093ff3
rgirodon <remy.girodon@gmail.com>
2019-04-07 12:08:25 -0400

    Set up Eloquent models for Club and Category

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = 'categories';
+    
+    public function club() {
+        
+        return $this->belongsTo('App\Club');
+    }
 }
