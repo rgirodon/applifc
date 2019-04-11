8f4ffebe9ebee2d5d5ca1aa9016ce8c5357e35d1
rgirodon <remy.girodon@gmail.com>
2019-04-11 06:25:55 -0400

    Add player page

--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -12,8 +12,15 @@
 
 @section('content')
 
-<div>
-    
-</div>
+<main>
+	<div class="panel panel-default">
+		<div class="panel-heading">{{ $club->name }} - {{ $player->getCurrentLicence()->category->label }}</div>
+        <div class="panel-body">        
+           	<p>Nom : {{ $player->lastname }}</p>
+            <p>Prénom : {{ $player->firstname }}</p>
+            <p>Date de naissance : {{ $player->birth }}</p>
+        </div>
+    </div>
+</main>
 
 @endsection
