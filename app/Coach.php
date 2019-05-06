e4b26920745b3c7e1cc63274aedbe2b0bc36a44e
rgirodon <remy.girodon@gmail.com>
2019-05-06 11:38:03 -0400

    Verify and customize forgot mail password

--- app/Coach.php
+++ app/Coach.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class Coach extends Authenticatable
 {
@@ -32,5 +34,22 @@ class Coach extends Authenticatable
     public function getFullName() {
         
         return $this->firstname.' '.$this->lastname;
+    }
+    
+    public function sendPasswordResetNotification($token) {
+        
+        $this->notify(new CustomPasswordResetEmail($token));
+    }
+}
+
+class CustomPasswordResetEmail extends ResetPassword {
+    
+    public function toMail($notifiable)
+    {
+        return (new MailMessage())
+        ->subject(env('APP_NAME').' - Réinitialisation de mot de passe')
+        ->line('Nous vous envoyons ce mail car nous avons reçu de votre part une demande de réinitialisation de mot de passe.')
+        ->action('Réinitialiser le mot de passe', url(config('app.url') . route('password.reset', $this->token, false)))
+        ->line('Si vous n\'avez pas demandé de réinitialisation de mot de passe, aucune action n\'est requise.');
     }
 }
