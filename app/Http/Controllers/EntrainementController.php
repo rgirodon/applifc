76cc9bcae535fde23763e68a8bb75b375c733712
khoucha <mook9005@gmail.com>
2019-05-20 15:49:29 +0200

    crud entrainement

--- app/Http/Controllers/EntrainementController.php
+++ app/Http/Controllers/EntrainementController.php
@@ -2,68 +2,219 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Entrainement;
+use Illuminate\Http\Request;
 use Carbon\Carbon;
-use App\Coach;
 use App\Category;
+use Illuminate\Support\Facades\Auth;
+use App\Club;
+use App\Player;
+use App\Coach;
 
 class EntrainementController extends Controller
 {
     public function index() {
-        
+
         $dateDebut = Carbon::now()->subDay(1);
-        
+
         $dateFin = Carbon::now()->addWeek(2);
-        
+
         $entrainements = Entrainement::retrieveEntrainementsForDefaultClub($dateDebut, $dateFin);
-        
+
         $coachs = Coach::retrieveCoachsForDefaultClub();
-        
+
         $categories = Category::retrieveCategoriesForDefaultClub();
-        
-        return view('entrainements')->with(compact('entrainements', 'coachs', 'categories'));
+
+        return view('entrainement.list')->with(compact('entrainement', 'coachs', 'categories'));
     }
-    
+
     public function show($id) {
-        
+
         $entrainement = Entrainement::find($id);
-            
-        return view('entrainement')
-                ->with(compact('entrainement'));
+
+        return view('entrainement.view')
+            ->with(compact('entrainement'));
     }
-    
+
     public function findByCoach($coachId) {
-        
+
         $dateDebut = Carbon::now()->subDay(1);
-        
+
         $dateFin = Carbon::now()->addWeek(2);
-        
-        $entrainements = Entrainement::retrieveEntrainementsForDefaultClub($dateDebut, $dateFin, $coachId, false);
-        
+
+        $entrainements = Entrainement::retrieveEntreprisesForDefaultClub($dateDebut, $dateFin, $coachId, false);
+
         $selectedCoach = Coach::find($coachId);
-        
+
         $coachs = Coach::retrieveCoachsForDefaultClub();
-        
+
         $categories = Category::retrieveCategoriesForDefaultClub();
-        
-        return view('entrainements')->with(compact('entrainements', 'coachs', 'selectedCoach', 'categories'));
+
+        return view('entrainement.list')->with(compact('entrainements', 'coachs', 'selectedCoach', 'categories'));
     }
-    
+
     public function findByCategory($categoryId) {
-        
-        $dateDebut = Carbon::now()->subWeek(2);
-        
+
+        $dateDebut = Carbon::now()->subDay(1);
+
         $dateFin = Carbon::now()->addWeek(2);
-        
+
         $entrainements = Entrainement::retrieveEntrainementsForDefaultClub($dateDebut, $dateFin, false, $categoryId);
-        
+
         $coachs = Coach::retrieveCoachsForDefaultClub();
-        
+
         $categories = Category::retrieveCategoriesForDefaultClub();
-        
+
         $selectedCategory = Category::find($categoryId);
-        
-        return view('entrainements')->with(compact('entrainements', 'coachs', 'categories', 'selectedCategory'));
+
+        return view('entrainement.list')->with(compact('entrainements', 'coachs', 'categories', 'selectedCategory'));
+    }
+
+    public function destroy(Request $request, $id) {
+
+        try {
+            $entrainement = Convocation::find($id);
+
+            $entrainement->delete();
+
+            $request->session()->flash('delete_message_ok', 'Entrainement supprimée');
+        }
+        catch(\Exception $exception) {
+
+            $request->session()->flash('delete_message_ko', 'Impossible de supprimer cette entrainement');
+        }
+
+        return redirect()->route('entrainement');
+    }
+
+    public function create() {
+
+        $categories = Category::retrieveCategoriesForDefaultClub();
+
+        $coach = Auth::user();
+
+        return view('entrainement.create')
+            ->with(compact('categories', 'coach'));
+    }
+
+    public function edit($id) {
+
+        $convocation = Convocation::find($id);
+
+        $categories = Category::retrieveCategoriesForDefaultClub();
+
+        $coach = Auth::user();
+
+        return view('convocation.edit')
+            ->with(compact('convocation', 'categories', 'coach'));
+    }
+
+    public function update(Request $request, $id) {
+
+        $request->validate([
+            'categoryIds' => 'bail|required|array|min:1',
+            'coach' => 'bail|required|integer',
+            'date_entrainement' => 'bail|required|date',
+
+
+        ]);
+
+        $coach = Coach::find($request->input('coach'));
+
+        $entrainement = Entrainement::find($id);
+
+        $entrainement->coach()->associate($coach);
+
+        $entrainement->date_entrainement = $request->input('date_entrainement');
+
+        $entrainement->save();
+
+        $categories = $entrainement->categories()->get();
+
+        foreach($categories as $category) {
+
+            $entrainement->categories()->detach($category);
+        }
+
+        $categoryIds = $request->input('categoryIds');
+
+        foreach ($categoryIds as $categoryId) {
+
+            $entrainement->categories()->attach($categoryId);
+        }
+
+        return redirect()->route('entrainement', $entrainement->id);
+    }
+
+    public function store(Request $request) {
+
+        $request->validate([
+            'categoryIds' => 'bail|required|array|min:1',
+            'coach' => 'bail|required|integer',
+            'date_entrainement' => 'bail|required|date',
+
+        ]);
+
+        $club = Club::findDefaultClub();
+
+        $coach = Coach::find($request->input('coach'));
+
+        $entrainement = new entrainement();
+
+        $entrainement->club()->associate($club);
+
+        $entrainement->coach()->associate($coach);
+
+        $entrainement->date_entrainement = $request->input('date_entrainement');
+
+        $entrainement->save();
+
+        $categoryIds = $request->input('categoryIds');
+
+        foreach ($categoryIds as $categoryId) {
+
+            $entrainement->categories()->attach($categoryId);
+        }
+
+        return redirect()->route('entrainement', $entrainement->id);
+    }
+
+    public function addPlayer(Request $request, $id) {
+
+        $request->validate([
+            'playerId' => 'bail|required|integer'
+        ]);
+
+        $playerId = $request->input('playerId');
+
+        $alreadyExistingEntrainement = Entrainement::where('id', '=', $id)
+            ->whereHas('players',
+                function ($query) use($playerId) {
+
+                    $query->where('players.id', '=', $playerId);
+                })
+            ->get();
+
+        if ($alreadyExistingEntrainement->isEmpty()) {
+
+            $entrainement = Convocation::find($id);
+
+            $player = Player::find($playerId);
+
+            $entrainement->players()->attach($player);
+        }
+
+        return redirect()->route('entrainement', $id);
+    }
+
+    public function deletePlayer(Request $request, $id, $playerId) {
+
+        $entrainement = Entrainement::find($id);
+
+        $player = Player::find($playerId);
+
+        $entrainement->players()->detach($player);
+
+        return redirect()->route('entrainement', $id);
     }
 }
