097ec396f5e8072d2e1b5282f3a642718a1aa333
khoucha <mook9005@gmail.com>
2019-07-12 10:53:52 +0200

    sup police

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,6 @@
 <html>
     <head>
         <title>Appli FC - @yield('title')</title>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}"></link>
         <link href="https://fonts.googleapis.com/css?family=Barriecito&display=swap" rel="stylesheet">
         <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 
